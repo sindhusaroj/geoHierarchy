0f8e722f133a4cbb39209a87a468985737a86efd
sindhusaroj <58171631+sindhusaroj@users.noreply.github.com>
2019-12-11 13:58:05 +0530

    getgeoHierarchy

--- getgeoHierarchy.py
+++ getgeoHierarchy.py
@@ -1,61 +1,66 @@
-5.04 //15/11/2019
+# Task Fetching the Data from the Csv file and export as the JSON format
+#Version : 1.0
 import csv
 import json
 import boto3
-
+import sys
+from botocore.exceptions import ClientError
 
 def geoHierarchy_handler(event, context):
+    
+    outputs = []
+    dictCreate = {}
     targetbucket = 'geohierarchys3'
     csvkey = 'GeoHierarchyReference.csv'
-    jsonkey = 'GeoHierarchyResponse.json'
+    jsonkey = 'test.json'
+    try:
+        s3 = boto3.resource('s3')
+        csv_object = s3.Object(targetbucket, csvkey)
+        csv_content =csv_object.get()['Body'].read().splitlines()
+        s3_client = boto3.client('s3')
+    except ClientError as ex:
+        if ex.response['Error']['Code'] == 'NoSuchKey':
+            print("The specified csv file does not exist!. Detail. {}".format(ex))
+       
 
-    s3 = boto3.resource('s3')
-    csv_object = s3.Object(targetbucket, csvkey)
-    csv_content = csv_object.get()['Body'].read().splitlines()
-    s3_client = boto3.client('s3')
-    
-    l= []
-    
-    for line in csv_content:
-        x = json.dumps(line.decode('utf-8')).split(',')
-        continentName = str(x[0])
-        countryCode = str(x[1])
-        countryName = str(x[2])
-        regionCode = str(x[3])
-        regionName = str(x[4])
-        destinationCode = str(x[5])
-        destinationName = str(x[6])
-        accomId = str(x[10])
-        accomName = str(x[11])
-        longitude = str(x[12])
-        latitude = str(x[13])
-        
-        
-        if event['countryName'] in countryName:
-            jsonCompose = '{ "continentName": ' + continentName + '"' + ','  \
-                + ' "countryCode": ' + '"' + countryCode + '"' + ',' \
-                + ' "countryName": ' + '"' + countryName + '"' + ',' \
-                + ' "regionCode": ' + '"' + regionCode + '"' + ',' \
-                + ' "regionName": ' + '"' + regionName + '"' + ',' \
-                + ' "destinationCode": ' + '"' + destinationCode + '"' + ',' \
-                + ' "destinationName": ' + '"' + destinationName + '"' + ',' \
-                + ' "accomId": ' + '"' + accomId + '"' + ',' \
-                + ' "accomName": ' + '"' + accomName + '"' + ',' \
-                + ' "longitude": ' + '"' + longitude + '"' + ',' \
-                + ' "latitude": ' + '"' + latitude + '"' + '}' 
-            l.append(jsonCompose)    
+    try:
+        for line in csv_content:
+            line =  line.decode('utf-8')
+            splitLine = line.split(',')
+            while len(splitLine[3]) <6:
+                splitLine[3]= "0" + (splitLine[3])
+            while len(splitLine[5]) <6:
+                splitLine[5]= "0" + (splitLine[5])  
+            while len(splitLine[10]) <6:
+                splitLine[10]= "0" + (splitLine[10])
+            dictCreate['continentName'] = str(splitLine[0])                      
+            dictCreate['countryCode'] = str(splitLine[1])
+            dictCreate['countryName'] = str(splitLine[2])
+            dictCreate['regionCode'] = str(splitLine[3])
+            dictCreate['regionName'] = str(splitLine[4])
+            dictCreate['destinationCode'] = str(splitLine[5])
+            dictCreate['destinationName'] = str(splitLine[6])
+            dictCreate["accomodation"] = [{ 'accomId' :str(splitLine[10]),
+            'accomName': str(splitLine[15]), 
+            'longitude' : str(splitLine[16]),
+            'latitude' : str(splitLine[17]),
+            "brand":{'TUIUKBrand' : str(splitLine[12])}}]
+            if dictCreate['countryName'] == event['countryName'] :
+                outputs.append(dictCreate.copy())
+
+
+    except Exception as e:
+        print("Failed reading file from bucket. Continuing. {}".format(e))
+
     s3_client.put_object(
         Bucket=targetbucket,
-        Body=str(l).replace("'", ""),
+        Body = str(outputs),
         Key=jsonkey,
         ServerSideEncryption='AES256'
     )
-    
     s3_obj =boto3.client('s3')
     s3_clientobj = s3_obj.get_object(Bucket='geohierarchys3', Key='GeoHierarchyResponse.json')
-    s3_clientdata = s3_clientobj['Body'].read().decode('utf-8')
+    s3_clientdata = s3_clientobj['Body'].read()
     s3clientlist=json.loads(s3_clientdata)
-
+    return {'geoHierarchyResponse':{"geoHierarchy":outputs }}
     
-    return {"geoHierarchyResponse":{'geoHierarchy': s3clientlist}}
-   
